a88a916e705a5cbdaf9a35dee4f2066cf63a36a4
yn_qian <yn_qian@yeah.net>
2018-10-10 23:12:21 +0800

    update

---
+++ learn-project-acm/src/main/java/net/stealthcat/acm/hdu/Problem1001.java
@@ -0,0 +1,14 @@
+package net.stealthcat.acm.hdu;
+
+import java.util.Scanner;
+
+public class Problem1001 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            
+        }
+    }
+
+}
