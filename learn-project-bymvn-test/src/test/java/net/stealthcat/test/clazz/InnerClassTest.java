895efc92c19d618b15205b04093bc04907e68ab4
qian.yang <qian.yang@chinaredstar.com>
2020-12-22 14:37:11 +0800

    add inner class test

--- learn-project-bymvn-test/src/test/java/net/stealthcat/test/clazz/InnerClassTest.java
+++ learn-project-bymvn-test/src/test/java/net/stealthcat/test/clazz/InnerClassTest.java
@@ -1,20 +1,22 @@
 package net.stealthcat.test.clazz;
 
 public class InnerClassTest {
-
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.field = "test";
+        Outer.Inner inner = outer.new Inner();
+        inner.print();
+    }
 }
 
 class Outer {
+    String field;
+
     public class Inner {
 
+        void print() {
+            System.out.println(field);
+        }
     }
 }
 
-class Other {
-
-    void test (){
-        Outer outer = new Outer();
-
-
-    }
-}
