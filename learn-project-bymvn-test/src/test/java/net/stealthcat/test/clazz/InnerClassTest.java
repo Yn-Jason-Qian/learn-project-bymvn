c4da7b8cec92ee2e0a897788218fee17a503a5ae
qian.yang <qian.yang@chinaredstar.com>
2020-12-22 14:25:33 +0800

    test inner class

---
+++ learn-project-bymvn-test/src/test/java/net/stealthcat/test/clazz/InnerClassTest.java
@@ -0,0 +1,20 @@
+package net.stealthcat.test.clazz;
+
+public class InnerClassTest {
+
+}
+
+class Outer {
+    public class Inner {
+
+    }
+}
+
+class Other {
+
+    void test (){
+        Outer outer = new Outer();
+
+
+    }
+}
